da250075e60116cc756698635844c064e1755140
DianaGakuya <deegakuya@gmail.com>
2024-08-13 14:26:36 +0300

    Your commit message here

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import app from './app.js';
+import logger from './configs/logger.js';
 
-// env variables
 const PORT = process.env.PORT || 8000;
-console.log(process.env.NODE_ENV);
+
 app.listen(PORT, () => {
-    console.log(`Server is listening at ${PORT}...`);
+  logger.info(`Server is listening at ${PORT}...`);
 });
+
 
  
